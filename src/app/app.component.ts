36922549e242d13a32df1434bb0d9031f8f8e9e4
fabio da silva <fabinhoevil16@hotmail.com>
2020-10-03 14:16:39 -0300

    projeto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {Pessoa} from './pessoa';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'angularaulas';
+  nomes = [
+    new Pessoa(1, 'Fabio', 90),
+    new Pessoa(2, 'Paulo', 10),
+    new Pessoa(3, 'Paula', 20),
+    new Pessoa(4, 'Felipe', 30),
+    new Pessoa(5, 'Larissa', 88)
+  ];
+  nomePrincipal = this.nomes[0]
+
+  showAviso = true;
+  avisoMsg = 'Este é um aviso!';
 }
