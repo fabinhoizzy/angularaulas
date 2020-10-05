8747d2e81a1a0a3d55457285688cf9c751a2d2b0
fabio da silva <fabinhoevil16@hotmail.com>
2020-10-05 13:27:54 -0300

    aplicação de teste, adicionar tarefas com o angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
-import {Pessoa} from './pessoa';
+import { Tarefa } from './tarefa';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angularaulas';
-  nomes = [
-    new Pessoa(1, 'Fabio', 90),
-    new Pessoa(2, 'Paulo', 10),
-    new Pessoa(3, 'Paula', 20),
-    new Pessoa(4, 'Felipe', 30),
-    new Pessoa(5, 'Larissa', 88)
-  ];
-  nomePrincipal = this.nomes[0]
+  title = 'Lista de Tarefas';
 
-  showAviso = true;
-  avisoMsg = 'Este é um aviso!';
+  tarefas = [
+    new Tarefa('Fazer Bolo'),
+    new Tarefa('Testar o app')
+  ];
+
+  addTarefa = function(tarefa: string) {
+    this.tarefas.push( new Tarefa(tarefa));
+  }
 }
